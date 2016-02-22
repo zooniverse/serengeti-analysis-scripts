447db819d2dbafd054e510be00fc584adc7214eb
Alex Bowyer <alex.bowyer@gmail.com>
2016-02-22 09:40:36 +0000

    bugfix

--- check-seasons-present-in-classifications.py
+++ check-seasons-present-in-classifications.py
@@ -30,6 +30,26 @@ client = pymongo.MongoClient()
 db = client['serengeti3']
 classification_collection = db["serengeti_classifications"]
 
+seasons = {
+  1: ObjectId("50c6197ea2fc8e1110000001"),
+  2: ObjectId("50c61e51a2fc8e1110000002"),
+  3: ObjectId("50c62517a2fc8e1110000003"),
+  4: ObjectId("50e477293ae740a45f000001"),
+  5: ObjectId("51ad041f3ae7401ecc000001"),
+  6: ObjectId("51f158983ae74082bb000001"),
+  7: ObjectId("5331cce91bccd304b6000001"),
+  8: ObjectId("54cfc76387ee0404d5000001"),
+  0: ObjectId("55a3d6cf3ae74036bc000001"),
+  9: ObjectId("56a63b3b41479b0042000001")
+}
+
+def get_season_no(oid):
+  season = -1
+  for i_season, i_oid in seasons.iteritems():
+    if oid == i_oid:
+      season = i_season
+  return season
+
 season_found = {
   1: False,
   2: False,
@@ -51,16 +71,17 @@ for ii, classification in enumerate(classification_collection.find(no_cursor_tim
   if ii % 10000 == 0:
     print ii
   subject_id = classification["subjects"][0]["zooniverse_id"]
-  season = subject_season_map[subject_id]
-  if not season_found[season]:
-    season_found[season]=True
+  season_oid = subject_season_map[subject_id]
+  season_num = get_season_no(season_oid)
+  if not season_found[season_num]:
+    season_found[season_num]=True
 
 print "\nClassification Season Check Results:\n"
-for season,status in season_found.iteritems():
+for season_num,status in season_found.iteritems():
   if status:
-    print "For Season %s, classifications were found."
+    print "For Season %s, classifications were found." % season_num
   else:
-    print "For Season %s, NO CLASSIFICATIONS FOUND."
+    print "For Season %s, NO CLASSIFICATIONS FOUND." % season_num
 
 print "\nDone.\n"
 
