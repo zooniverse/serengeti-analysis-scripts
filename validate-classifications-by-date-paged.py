f374fb8993c409c0b8d344d7e53e9173108cf70c
Alex Bowyer <alex.bowyer@gmail.com>
2016-02-22 14:14:49 +0000

    code simplification and optimisation

--- validate-classifications-by-date-paged.py
+++ validate-classifications-by-date-paged.py
@@ -11,22 +11,19 @@ classification_collection = db["serengeti_classifications"]
 
 print "Collection contains %s documents." % db.command("collstats", "serengeti_classifications")["count"]
 
-pageSize = 5000
+pageSize = 100000
+first_classification = classification_collection.find_one()
+completed_page_rows=1
+last_id = first_classification["_id"]
 
-page_one = enumerate(classification_collection.find().limit(pageSize))
-completed_page_rows = 0
-for ii, classification in page_one:
-  completed_page_rows+=1
-  last_id = classification["_id"]
-
-next_results = classification_collection.find({"_id":{"$gt":last_id}},no_cursor_timeout=True).limit(pageSize)
+next_results = classification_collection.find({"_id":{"$gt":last_id}},{"created_at":1},no_cursor_timeout=True).limit(pageSize)
 while next_results.count()>0:
   for ii, classification in enumerate(next_results):
     completed_page_rows+=1
-    if completed_page_rows % 10000 == 0 or completed_page_rows > 10593400:
+    if completed_page_rows % pageSize == 0:
       print "%s (id = %s): created at %s" % (completed_page_rows,classification["_id"],classification["created_at"])
     last_id = classification["_id"]
-  next_results = classification_collection.find({"_id":{"$gt":last_id}}).limit(pageSize)
+  next_results = classification_collection.find({"_id":{"$gt":last_id}},{"created_at":1},no_cursor_timeout=True).limit(pageSize)
 
 print "\nDone.\n"
 
